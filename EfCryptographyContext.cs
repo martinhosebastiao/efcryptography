e6ddce5a235f3ba01f769f4d123276f88922e585
Martinho Sebastião <martinho.sebastiao@outlook.com>
2023-10-28 19:03:49 +0100

    Configuração inicial
    Aplicação do entity framework e criação da classe de criptografia

---
+++ EfCryptographyContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCryptography
+{
+    public class EfCryptographyContext : DbContext
+    {
+        public DbSet<User> Users { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=mssql.mas.ao,4433;Initial Catalog=EFCrypto;User Id=sa;Password=M@rtina2019;Persist Security Info=False;Trusted_Connection=False;Encrypt=False;TrustServerCertificate=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                        .ToTable("User")
+                        .Property(e => e.UserId).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<User>()
+                        .Property(e => e.Password)
+                        .HasConversion<Cryptography>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
